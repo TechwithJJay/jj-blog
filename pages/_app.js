ec4d710185a601405ac4f0ec65697d9609b2c2af
Sajid, Muhammad Junaid | Jay | TMO <muhammadjunaid.sajid@rakuten.com>
2023-06-14 15:06:47 +0900

    GraphQL data fetching done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import React, { useEffect, useState } from 'react';
+import { Layout } from '../components';
+import '@/styles/globals.scss';
+import 'tailwindcss/tailwind.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
+
+export default App;
