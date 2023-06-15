eda8d2dfe277b33fa0f19826bee80d81debb508f
Sajid, Muhammad Junaid | Jay | TMO <muhammadjunaid.sajid@rakuten.com>
2023-06-15 17:47:30 +0900

    completed the author

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
-const Author = () => {
-  return (
-    <div>Author</div>
-  )
-}
+// import { grpahCMSImageLoader } from '../util';
 
-export default Author
+const Author = ({ author }) => (
+  <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+    <div className="absolute left-0 right-0 -top-14">
+      <Image
+        // loader={grpahCMSImageLoader}
+        alt={author.name}
+        unoptimized
+        height="100"
+        width="100"
+        className="align-middle rounded-full"
+        src={author.photo.url}
+      />
+    </div>
+    <h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
+    <p className="text-white text-ls">{author.bio}</p>
+  </div>
+);
+
+export default Author;
