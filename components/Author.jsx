0a31cc9d3c521d8ccb249842d40fe26709d2286d
Sajid, Muhammad Junaid | Jay | TMO <muhammadjunaid.sajid@rakuten.com>
2023-06-22 15:03:56 +0900

    fixed the typos and build it

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
 
-import { grpahCMSImageLoader } from '../util.js';
+import { graphCMSImageLoader } from '../util.js';
 
 const Author = ({ author }) => (
   <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
     <div className="absolute left-0 right-0 -top-14">
       <Image
-        unoptimized
-        loader={grpahCMSImageLoader}
+        loader={graphCMSImageLoader}
         alt={author.name}
         height={100}
         width={100}
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import moment from 'moment';
 import Link from 'next/link';
 
-import { grpahCMSImageLoader } from '../util';
+import { graphCMSImageLoader } from '../util';
 import { getSimilarPosts, getRecentPosts } from '../services';
 
 const PostWidget = ({ categories, slug }) => {
@@ -19,7 +19,7 @@ const PostWidget = ({ categories, slug }) => {
         setRelatedPosts(result);
       });
     }
-  }, []);
+  }, [categories, slug]);
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
@@ -30,11 +30,10 @@ const PostWidget = ({ categories, slug }) => {
         <div key={post.title} className="flex items-center w-full mb-4">
           <div className="w-16 flex-none">
             <Image
-              // loader={grpahCMSImageLoader}
+              loader={graphCMSImageLoader}
               alt={post.title}
               height={60}
               width={60}
-              unoptimized
               className="align-middle rounded-full"
               src={post.featuredImage.url}
             />
