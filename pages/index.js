f3b53f8d02182b07b04af426ee845020073e16be
Sajid, Muhammad Junaid | Jay | TMO <muhammadjunaid.sajid@rakuten.com>
2023-07-05 11:13:34 +0900

    readded the categories

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ posts }) {
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
-            {/* <Categories /> */}
+            <Categories />
           </div>
         </div>
       </div>
