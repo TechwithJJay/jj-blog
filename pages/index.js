0bb8e6339613f161f185d1ad0ff9b47b2591a8a6
Sajid, Muhammad Junaid | Jay | TMO <muhammadjunaid.sajid@rakuten.com>
2023-07-05 11:08:24 +0900

    removed the categories

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import { getPosts } from '../services';
 
 export default function Home({ posts }) {
+  // console.table(posts[0].node);
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -21,7 +22,7 @@ export default function Home({ posts }) {
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
-            <Categories />
+            {/* <Categories /> */}
           </div>
         </div>
       </div>
