a590cc15af159497f5ae82356d00197bcd812204
Sajid, Muhammad Junaid | Jay | TMO <muhammadjunaid.sajid@rakuten.com>
2024-06-21 17:20:45 +0900

    update name

--- components/Header.jsx
+++ components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Jay Tech Blogs
+              Junaid Rao Blogs
             </span>
           </Link>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Jay Tech Blogs</title>
+        <title>Junaid Rao Blogs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <FeaturedPosts />
